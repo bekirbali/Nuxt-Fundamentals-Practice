046c93bedbdfeb6ef0bbf5ae82cc190759f6b7d8
bekirbali <bfbali43@gmail.com>
2023-03-27 03:51:48 +0300

    animated icons

--- components/Form.vue
+++ components/Form.vue
@@ -1,7 +1,13 @@
 <template>
   <form @submit="submitHandler">
     <label for="city">Search</label>
-    <input type="text" name="city" @change="searchText" v-model="search" />
+    <input
+      type="text"
+      name="city"
+      @change="searchText"
+      v-model="search"
+      placeholder="Search a city"
+    />
     <button type="submit">Submit</button>
   </form>
   <DisplayWeatherData :data="weatherData" />
@@ -17,13 +23,11 @@ export default {
   methods: {
     submitHandler(e) {
       e.preventDefault();
-      const { data } = this.fetcher(this.search);
-
+      this.fetcher(this.search);
       this.search = "";
     },
     searchText(e) {
       this.search = e.target.value;
-      console.log(this.search);
     },
   },
   props: {
@@ -32,3 +36,34 @@ export default {
   },
 };
 </script>
+
+<style scoped lang="scss">
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  button {
+    width: 50%;
+    border: none;
+    border-radius: 0.5rem;
+    padding: 0.3rem;
+    background-color: cadetblue;
+    color: white;
+    &:hover {
+      background-color: rgb(90, 205, 190);
+    }
+    &:active {
+      transform: translate(0.05px, 0.5px);
+
+      background-color: rgb(73, 116, 117);
+    }
+  }
+  input {
+    padding: 0.3rem 0.5rem;
+    border: none;
+    border-radius: 0.5rem;
+    text-transform: capitalize;
+  }
+}
+</style>
